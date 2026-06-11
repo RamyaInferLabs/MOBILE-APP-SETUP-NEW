925860aff4f7812debd894fe568082f6bf4999aa
Ramya <ramya89780@gmail.com>
2026-06-11 17:59:20 +0530

    custom fonts usage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,12 +12,15 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function Index() {
   return (
     <SafeAreaView className = 'flex-1 bg-background p-5' >
-      <Text className="text-xl font-bold text-success">
-        welcome to nativewind
+      <Text className="text-5xl font-sans-extrabold ">
+            Home
       </Text>
-      <Link href = "/onboarding" className="mt-4 rounded bg-primary text-white p-4">go to onboarding</Link>
-      <Link href = "/(auth)/SingIn" className="mt-4 rounded bg-primary text-white p-4">Log In</Link>
-     <Link href = "/(auth)/SingUp"className="mt-4 rounded bg-primary text-white p-4" >Sign Up</Link>
+      {/* <Text className="text-7xl font-bold ">
+            Home
+      </Text> */}
+      <Link href = "/onboarding" className="mt-4  font-sans-bold rounded bg-primary text-white p-4">go to onboarding</Link>
+      <Link href = "/(auth)/SingIn" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Log In</Link>
+     <Link href = "/(auth)/SingUp"className="mt-4  font-sans-bold rounded bg-primary text-white p-4" >Sign Up</Link>
 
 
        
---
+++ constants/images.ts
@@ -0,0 +1,1 @@
+// import splashPattern from '@/assets/images/splash-pattern.png'
